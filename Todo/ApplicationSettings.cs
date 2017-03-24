531c699fab47e26f9baeb8688d29927d7816396b
Radu M <radu@DX-MacBook-Pro.local>
2017-03-24 14:21:00 +0200

    Added ApplicationSettings class to handle JSON settings

---
+++ Todo/ApplicationSettings.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Todo
+{
+	public class ApplicationSettings
+	{
+		private dynamic _settings { get; set; }
+
+		public ApplicationSettings()
+		{
+			var assembly = typeof(ApplicationSettings).GetTypeInfo().Assembly;
+			Stream stream = assembly.GetManifestResourceStream("Todo.appSettings.json");
+
+			string settings;
+			using (var streamReader = new StreamReader(stream))
+			{
+				settings = streamReader.ReadToEnd();
+			}
+			_settings = JsonConvert.DeserializeObject(settings);
+		}
+		public string GetSetting(string settingName)
+		{
+			var value = _settings[settingName].ToString();
+			return value;
+		}
+	}
+}
