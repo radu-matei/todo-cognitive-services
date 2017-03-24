24bf3af70f3246259f7cdf932f21396c7b63219a
Radu M <radu@DX-MacBook-Pro.local>
2017-03-24 14:21:16 +0200

    Reacted to the addition of ApplicationSettings

--- Todo/Constants.cs
+++ Todo/Constants.cs
@@ -1,1 +1,23 @@
-﻿
+﻿namespace Todo
+{
+	public static class Constants
+	{
+		private static ApplicationSettings _applicationSettings = new ApplicationSettings();
+
+		public static string AuthenticationTokenEndpoint => _applicationSettings.GetSetting("AuthenticationTokenEndpoint");
+
+		public static string BingSpeechApiKey => _applicationSettings.GetSetting("BingSpeechApiKey");
+		public static string SpeechRecognitionEndpoint => _applicationSettings.GetSetting("SpeechRecognitionEndpoint");
+		public static string AudioContentType => _applicationSettings.GetSetting("AudioContentType");
+
+		public static string BingSpellCheckApiKey => _applicationSettings.GetSetting("BingSpellCheckApiKey");
+		public static string BingSpellCheckEndpoint => _applicationSettings.GetSetting("BingSpellCheckEndpoint");
+
+		public static string TextTranslatorApiKey => _applicationSettings.GetSetting("TextTranslatorApiKey");
+		public static string TextTranslatorEndpoint => _applicationSettings.GetSetting("TextTranslatorEndpoint");
+
+		public static string EmotionApiKey => _applicationSettings.GetSetting("EmotionApiKey");
+
+		public static string AudioFilename => _applicationSettings.GetSetting("AudioFilename");
+	}
+}
